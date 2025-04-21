0d0cfd6ebb0f9218a6fb1e35d4ff2307db8b4a1c
eiman60 <421202305@qu.edu.sa>
2025-04-22 01:18:20 +0300

    First project upload

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.project.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setSelectedItemId(R.id.home);
+        replaceFragment(new homeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new homeFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new profileFragment());
+            } else if (itemId == R.id.cart) {
+                replaceFragment(new cartFragment());
+            } else if (itemId == R.id.subscribe) {
+                replaceFragment(new subscribeFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
